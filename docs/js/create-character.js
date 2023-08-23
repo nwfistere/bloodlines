6c4e87473d2cb289771df2145e8e009b25ff4df7
Nick Fistere <nwfistere@gmail.com>
2023-08-23 15:00:11 -0500

    frames work?

--- docs/js/create-character.js
+++ docs/js/create-character.js
@@ -98,6 +98,8 @@ const createJsonOutput = (form) => {
     json = JSON.parse(currentJson.textContent);
   }
 
+  const gotField = [];
+
   const char = json.character[0];
   for (let field of form.elements) {
     if (!field.id || field.disabled || field.type === "reset" || field.type === "submit" || field.type === "button") {
@@ -154,6 +156,15 @@ const createJsonOutput = (form) => {
       }
 
       const skinObject = char.skins[num];
+
+      if (skinField === "frames") {
+        if (!gotField.includes(skinFieldId)) {
+          skinObject[skinField] = getFieldValue({ type: "file", files: currentFiles.get(skinFieldId) || [] })
+          gotField.push(skinFieldId);
+        }
+        continue;
+      }
+
       const fieldValue = getFieldValue(field);
 
       skinObject[skinField] = fieldValue;
@@ -590,97 +601,63 @@ const hide = (elementId) => {
 
 const handleFrameDropbox = (container) => {
 
-	// modify all of the event types needed for the script so that the browser
-	// doesn't open the image in the browser tab (default behavior)
-	['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evt => {
-		container.addEventListener(evt, prevDefault, false);
-	});
-	function prevDefault (e) {
-		e.preventDefault();
-		e.stopPropagation();
-	}
+  // modify all of the event types needed for the script so that the browser
+  // doesn't open the image in the browser tab (default behavior)
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evt => {
+    container.addEventListener(evt, prevDefault, false);
+  });
+  function prevDefault (e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
-	// remove and add the hover class, depending on whether something is being
-	// actively dragged over the box area
-	['dragenter', 'dragover'].forEach(evt => {
-		container.addEventListener(evt, hover, false);
-	});
-	['dragleave', 'drop'].forEach(evt => {
-		container.addEventListener(evt, unhover, false);
-	});
-	function hover(e) {
-		container.classList.add('hover');
-	}
-	function unhover(e) {
-		container.classList.remove('hover');
-	}
+  // remove and add the hover class, depending on whether something is being
+  // actively dragged over the box area
+  ['dragenter', 'dragover'].forEach(evt => {
+    container.addEventListener(evt, hover, false);
+  });
+  ['dragleave', 'drop'].forEach(evt => {
+    container.addEventListener(evt, unhover, false);
+  });
+  function hover(e) {
+    container.classList.add('hover');
+  }
+  function unhover(e) {
+    container.classList.remove('hover');
+  }
 
-	// the DataTransfer object holds the data being dragged. it's accessible
-	// from the dataTransfer property of drag events. the files property has
-	// a list of all the files being dragged. put it into the filesManager function
-	container.addEventListener('drop', mngDrop, false);
-	function mngDrop(e) {
-		let dataTrans = e.dataTransfer;
-		let files = dataTrans.files;
-		filesManager(container.id, files);
-	}
-}
-
-// use FormData browser API to create a set of key/value pairs representing
-// form fields and their values, to send using XMLHttpRequest.send() method.
-// Uses the same format a form would use with multipart/form-data encoding
-function upFile(file) {
-  //only allow images to be dropped
-  let imageType = /image.*/;
-  if (file.type.match(imageType)) {
-    let url = 'HTTP/HTTPS URL TO SEND THE DATA TO';
-    // create a FormData object
-    let formData = new FormData();
-    // add a new value to an existing key inside a FormData object or add the
-    // key if it doesn't exist. the filesManager function will loop through
-    // each file and send it here to be added
-    formData.append('file', file);
-
-    // standard file upload fetch setup
-    fetch(url, {
-      method: 'put',
-      body: formData
-    })
-    .then(response => response.json())
-    .then(result => { console.log('Success:', result); })
-    .catch(error => { console.error('Error:', error); });
-  } else {
-    console.error("Only images are allowed!", file);
+  container.addEventListener('drop', mngDrop, false);
+  function mngDrop(e) {
+    let dataTrans = e.dataTransfer;
+    let files = dataTrans.files;
+    filesManager(container.id, files);
   }
 }
-
 
 // use the FileReader API to get the image data, create an img element, and add
 // it to the gallery div. The API is asynchronous so onloadend is used to get the
 // result of the API function
 function previewFile(idPrefix, file) {
-  // only allow images to be dropped
+
   let imageType = /image.*/;
   if (file.type.match(imageType)) {
     let fReader = new FileReader();
     let gallery = document.getElementById(`${idPrefix}-gallery`);
-    // reads the contents of the specified Blob. the result attribute of this
-    // with hold a data: URL representing the file's data
+    
     fReader.readAsDataURL(file);
-    // handler for the loadend event, triggered when the reading operation is
-    // completed (whether success or failure)
+
     fReader.onloadend = function() {
       let wrap = document.createElement('div');
       wrap.classList.add("file-label", "file-label--active", "col-2");
+
       let img = document.createElement('img');
       img.classList.add("frame-img");
-      // set the img src attribute to the file's contents (from read operation)
       img.src = fReader.result;
+
       let imgCapt = document.createElement('p');
-      // the name prop of the file contains the file name, and the size prop
-      // the file size. convert bytes to KB for the file size
       let fSize = (file.size / 1000) + ' KB';
       imgCapt.innerHTML = `<span class="fName">${file.name}</span><span class="fSize">${fSize}</span><span class="fType">${file.type}</span>`;
+
       gallery.appendChild(wrap).appendChild(img);
       gallery.appendChild(wrap).appendChild(imgCapt);
     }
@@ -691,6 +668,20 @@ function previewFile(idPrefix, file) {
       rmButton.type = "button";
       rmButton.id = `${idPrefix}-rm-button`;
       rmButton.value = `Remove All Images`;
+      rmButton.addEventListener("click", (ev) => {
+        const button = ev.target;
+        const prefix = button.id.substring(0, button.id.indexOf("-rm-button"));
+        const inputId = `${prefix}-input`;
+        const galleryId = `${prefix}-gallery`;
+        const input = document.getElementById(inputId);
+        const gall = document.getElementById(galleryId);
+        // Remove contents of Frames
+        input.value = "";
+        gall.innerHTML = "";
+        // Remove this button too.
+        document.getElementById(`${prefix}-rm-container`).innerHTML = "";
+        currentFiles.set(idPrefix, []);
+      });
       rmContainer.appendChild(rmButton);
     }
 
@@ -699,15 +690,25 @@ function previewFile(idPrefix, file) {
   }
 }
 
+const currentFiles = new Map();
+console.debug("new map");
+
 function filesManager(idPrefix, files) {
-  // spread the files array from the DataTransfer.files property into a new
-  // files array here
+  if (!currentFiles.has(idPrefix)) {
+    currentFiles.set(idPrefix, []);
+  }
+  console.debug(`Before: currentFiles[${idPrefix}] - ${currentFiles.get(idPrefix)}`);
   files = [...files];
-  // send each element in the array to both the upFile and previewFile
-  // functions
-  // TODO: instead of upFile, add to a list of files somewhere...
-  // files.forEach((file) => upFile(idPrefix, file));
   files.forEach((file) => previewFile(idPrefix, file));
+
+  currentFiles.get(idPrefix).push(...files);
+
+  // Update output:
+  const form = document.querySelector("#skins");
+  if (form) {
+    createJsonOutput(form);
+  }
+  console.debug(`After: currentFiles[${idPrefix}] - ${currentFiles.get(idPrefix)}`);
 }
 
 // Onload
